74ff33d48a667929dfd8b57a6ba4c457a7139169
adrian musa alfauzan <adrianmusaalfauzan@gmail.com>
2025-01-04 22:52:09 +0700

    update dashboard admin dan navbar

---
+++ components/sidebar.js
@@ -0,0 +1,215 @@
+"use client";
+import React, { useState } from "react";
+import { Card, Typography, List, ListItem, ListItemPrefix, Accordion, AccordionHeader, AccordionBody } from "@material-tailwind/react";
+import {
+  HomeIcon,
+  UserGroupIcon,
+  UserCircleIcon,
+  BuildingOffice2Icon,
+  PresentationChartBarIcon,
+  InformationCircleIcon,
+  CircleStackIcon,
+  ClockIcon,
+  ChartBarSquareIcon,
+  DocumentTextIcon,
+  CreditCardIcon,
+  DocumentPlusIcon,
+  Cog6ToothIcon,
+  PowerIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+
+const profilAdmin = require("@/assets/images/profil.jpg");
+function Sidebar() {
+  const [buka, setBuka] = useState(0);
+
+  const tanganiBuka = (nilai) => {
+    setBuka(buka === nilai ? 0 : nilai);
+  };
+
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 flex flex-col">
+      <div className="mb-4 p-2">
+        <Typography variant="h4" color="blue-gray">
+          SARJANA GEPREK
+        </Typography>
+      </div>
+      <hr className="border border-gray-300 w-72 self-center" />
+
+      {/* List Utama dengan justify-between */}
+      <List className="flex flex-col justify-between flex-grow ">
+        {/* Bagian Menu */}
+        <div>
+          <ListItem className="bg-orange-400 text-white">
+            <ListItemPrefix>
+              <HomeIcon className="h-5 w-5" />
+            </ListItemPrefix>
+            Beranda
+          </ListItem>
+
+          {/* Dropdown: Partisipan */}
+          <Accordion open={buka === 1} className=" py-1 ">
+            <ListItem className="p-0" onClick={() => tanganiBuka(1)}>
+              <AccordionHeader className="p-3 border-none">
+                <ListItemPrefix>
+                  <UserGroupIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                <Typography color="blue-gray" className="mr-auto font-normal">
+                  Partisipan
+                </Typography>
+                <ChevronDownIcon strokeWidth={2.5} className={`ml-auto h-5 w-5 transition-transform ${buka === 1 ? "rotate-180" : ""}`} />
+              </AccordionHeader>
+            </ListItem>
+            <AccordionBody className="py-1">
+              <List className="p-0">
+                <ListItem>
+                  <ListItemPrefix>
+                    <UserCircleIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Admin
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <UserIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Pengguna
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <BuildingOffice2Icon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Perusahaan
+                </ListItem>
+              </List>
+            </AccordionBody>
+          </Accordion>
+
+          {/* Dropdown: Produk */}
+          <Accordion open={buka === 2} className=" py-1">
+            <ListItem className="p-0" onClick={() => tanganiBuka(2)}>
+              <AccordionHeader className="p-3 border-none">
+                <ListItemPrefix>
+                  <PresentationChartBarIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                <Typography color="blue-gray" className="mr-auto font-normal">
+                  Produk
+                </Typography>
+                <ChevronDownIcon strokeWidth={2.5} className={`ml-auto h-5 w-5 transition-transform ${buka === 2 ? "rotate-180" : ""}`} />
+              </AccordionHeader>
+            </ListItem>
+            <AccordionBody className="py-1">
+              <List className="p-0">
+                <ListItem>
+                  <ListItemPrefix>
+                    <InformationCircleIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Informasi
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <CircleStackIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Jasa
+                </ListItem>
+              </List>
+            </AccordionBody>
+          </Accordion>
+
+          {/* Dropdown: Aktivitas */}
+          <Accordion open={buka === 3} className=" py-1">
+            <ListItem className="p-0" onClick={() => tanganiBuka(3)}>
+              <AccordionHeader className="p-3 border-none">
+                <ListItemPrefix>
+                  <ClockIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                <Typography color="blue-gray" className="mr-auto font-normal">
+                  Aktivitas
+                </Typography>
+                <ChevronDownIcon strokeWidth={2.5} className={`ml-auto h-5 w-5 transition-transform ${buka === 3 ? "rotate-180" : ""}`} />
+              </AccordionHeader>
+            </ListItem>
+            <AccordionBody className="py-1">
+              <List className="p-0">
+                <ListItem>
+                  <ListItemPrefix>
+                    <ChartBarSquareIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  IKM
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <DocumentTextIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Pengajuan
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <CreditCardIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Pembayaran
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <DocumentPlusIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Pembuatan
+                </ListItem>
+              </List>
+            </AccordionBody>
+          </Accordion>
+        </div>
+
+        {/* Bagian Pengaturan dan Profil */}
+        <div>
+          <Accordion open={buka === 4} className=" py-1">
+            <ListItem className="p-0" onClick={() => tanganiBuka(4)}>
+              <AccordionHeader className="p-3">
+                <ListItemPrefix>
+                  <Cog6ToothIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                <Typography color="blue-gray" className="mr-auto font-normal">
+                  Pengaturan
+                </Typography>
+                <ChevronDownIcon strokeWidth={2.5} className={`ml-auto h-5 w-5 transition-transform ${buka === 4 ? "rotate-180" : ""}`} />
+              </AccordionHeader>
+            </ListItem>
+            <AccordionBody className="py-1">
+              <List className="p-0">
+                <ListItem>
+                  <ListItemPrefix>
+                    <UserIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Profil Saya
+                </ListItem>
+                <ListItem>
+                  <ListItemPrefix>
+                    <PowerIcon className="h-5 w-5" />
+                  </ListItemPrefix>
+                  Keluar
+                </ListItem>
+              </List>
+            </AccordionBody>
+          </Accordion>
+
+          {/* Profile Section */}
+          <div className="mt-4 flex flex-col items-center">
+            <div className="relative">
+              <Image src={profilAdmin} width={60} height={60} alt="Profile Picture" className="rounded-full bg-yellow-300" />
+              <div className="absolute bottom-0 right-0 bg-green-500 w-3 h-3 rounded-full" />
+            </div>
+            <Typography variant="h6" className="mt-2">
+              Nama Anda
+            </Typography>
+            <Typography color="blue-gray" className="text-sm">
+              Role
+            </Typography>
+          </div>
+        </div>
+      </List>
+    </Card>
+  );
+}
+
+export default Sidebar;
